c05b543dcbb73a937d8400cd9aaea94cb62e9174
channy143 <channy143@users.noreply.github.com>
2026-07-03 22:45:07 +0800

    Fix Vercel serverless handler with cached http server

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,9 @@
+import { createServer } from 'http';
 import app from '../server/src/app';
-export default app;
+
+let server: ReturnType<typeof createServer>;
+
+export default function handler(req: any, res: any) {
+  if (!server) server = createServer(app);
+  server.emit('request', req, res);
+}
